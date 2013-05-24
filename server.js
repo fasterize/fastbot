1c0eea4076e2531288e5a4b514858241b846b81f
stefounet <s@fasterize.com>
2013-05-25 01:25:09 +0200

    More useful message from bot

--- server.js
+++ server.js
@@ -17,19 +17,19 @@ var admins = ['david','stef','abarre'];
 var chans = {};
 
 var bot = new irc.Client(server, botName, config);
-bot.send('PASS fast');
+bot.send('PASS', 'fast');
 
 bot.on('registered', function(msg) {
   bot.say('#Fast','hello, my name is fastbot ;-)');
 });
 
-bot.on("message", function(from, to, text, message) {
-  //console.log(message);
-  var chan = message.args[0];
-  if (chan === '#support' && !isAdminPresentIn(chan) ) {
-    bot.say('#Fast', "Someone spoke on the support chan !")
+bot.on("message#support", function(nick, text, message) {
+  if (!isAdminPresentIn('#support') ) {
+    bot.say('#Fast', nick + " spoke on the support chan ! It says : '" + text + "'")
   }
-  bot.say('#Fast', "¿Que?");
+});
+bot.on('pm', function(nick, text, message) {
+  bot.say(nick, "¿Que?");
 });
 
 bot.on('names', function (channel, nicks) {
